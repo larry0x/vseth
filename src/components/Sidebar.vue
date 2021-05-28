48d315a3a5d43538ff5e03c7f34d48de9384acbe
0xlarry <26318510+0xlarry@users.noreply.github.com>
2021-05-27 19:59:55 -0500

    Adjust margins

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -27,7 +27,7 @@
 
     <transition name="fade">
       <div id="sidebarExtended" v-if="showExtended">
-        <div class="d-flex align-items-center mt-2 mb-4">
+        <div class="d-flex align-items-center pe-3 mb-4">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
